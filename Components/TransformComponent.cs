905ad571fd94aed73ec4e37722fbcbca98846641
Mateusz Giza <matgizq@gmail.com>
2018-07-03 09:26:02 +0200

    Add Center and Origin(modifiable) properties to TransformComponent

--- Components/TransformComponent.cs
+++ Components/TransformComponent.cs
@@ -4,8 +4,16 @@ namespace SGame
 {
     public class TransformComponent : IComponent
     {
+        private Vector2? originOffset;
+
         public Entity Entity { get; set; }
         public Vector2 Position { get; set; }
         public Vector2 Size { get; set; }
+        public Vector2 Center => Position + (Size / 2);
+        public Vector2 Origin
+        {
+            get => originOffset.HasValue ? Position + originOffset.Value : Position + Center;
+            set => originOffset = value;
+        }
     }
 }
