13f6d8982faab831accb2392e1cd59cbe27f164a
Unknown <matgizq@gmail.com>
2018-07-05 22:35:10 +0200

    Add lighting masks and light effect shader

--- Common/Names/Textures.cs
+++ Common/Names/Textures.cs
@@ -6,5 +6,7 @@ namespace SGame.Common.Names
 
         public const string Ball = Directory + "ball";
         public const string Brick = Directory + "brick";
+        public const string LightMask = Directory + "lightmask";
+        public const string LightMask2 = Directory + "lightmask-2";
     }
 }
